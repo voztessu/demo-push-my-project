f627f506977a743a3ddfdedb5873a3b0a084f931
Lap trinh cung Kai <phunam1502@gmail.com>
2024-09-15 04:30:59 +0800

    test hello word

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+package hoidanit;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World with java");
+		System.out.println("Hello World with java");
+		System.out.println("Hello World with java");
+		System.out.println("Hello World with java");
+		System.out.println("Hello World with java");
+	}
+}
